44ddeb774dc904c577169ea794435cfe761f081a
Tatyana Thomas <tatyanathomas@Tatyanas-MBP.lan>
2026-05-09 22:26:25 -0400

    added cognito oath for testing

--- modules/auth/main.tf
+++ modules/auth/main.tf
@@ -20,4 +20,17 @@ resource "aws_cognito_user_pool_client" "web_client" {
 
   # Best practice for modern web apps using Cognito
   explicit_auth_flows = ["ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+  
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_scopes                 = ["openid", "email", "profile"]
+  supported_identity_providers         = ["COGNITO"]
+
+  callback_urls = ["http://localhost:3000/callback.html"]
+  logout_urls   = ["http://localhost:3000/"]
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "myteam-app"
+  user_pool_id = aws_cognito_user_pool.main_pool.id
 }
