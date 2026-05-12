7b8a067d8c150df3e6541261bfd43f3eef4f80ba
Tatyana Thomas <tatyanathomas@Tatyanas-MBP.lan>
2026-05-12 19:48:30 -0400

    added proxy to enable docs

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -186,7 +186,8 @@ resource "aws_api_gateway_deployment" "team_diamonds_deployment" {
     aws_api_gateway_integration.oauth_lambda_integration,
     aws_api_gateway_integration_response.oauth_options_integration_response,
     aws_api_gateway_integration.actions_lambda_integration,
-    aws_api_gateway_integration_response.actions_options_integration_response
+    aws_api_gateway_integration_response.actions_options_integration_response,
+    aws_api_gateway_integration.proxy_lambda_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.team_diamonds_api.id
@@ -204,7 +205,11 @@ resource "aws_api_gateway_deployment" "team_diamonds_deployment" {
       aws_api_gateway_method.actions_method.id,
       aws_api_gateway_integration.actions_lambda_integration.id,
       aws_api_gateway_method.actions_options.id,
-      aws_api_gateway_integration.actions_options_mock.id
+      aws_api_gateway_integration.actions_options_mock.id,
+
+      aws_api_gateway_resource.proxy_resource.id,
+      aws_api_gateway_method.proxy_method.id,
+      aws_api_gateway_integration.proxy_lambda_integration.id
     ]))
   }
 
@@ -217,4 +222,27 @@ resource "aws_api_gateway_stage" "prod_stage" {
   deployment_id = aws_api_gateway_deployment.team_diamonds_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.team_diamonds_api.id
   stage_name    = "prod"
+}
+
+# /{proxy+} enables /docs, /redoc, etc.
+resource "aws_api_gateway_resource" "proxy_resource" {
+  rest_api_id = aws_api_gateway_rest_api.team_diamonds_api.id
+  parent_id   = aws_api_gateway_rest_api.team_diamonds_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.team_diamonds_api.id
+  resource_id   = aws_api_gateway_resource.proxy_resource.id
+  http_method   = "ANY"
+  authorization = "NONE"   # No auth so /docs is publicly accessible
+}
+
+resource "aws_api_gateway_integration" "proxy_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.team_diamonds_api.id
+  resource_id             = aws_api_gateway_resource.proxy_resource.id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.action_lambda_invoke_arn
 }
